31b9f20691b0be5c8ca987f48f29e1873a8123f6
Yusuf Bukhari <yusufbukhari@gmail.com>
2026-03-17 17:22:37 +0000

    Implement simple API with two endpoints

---
+++ rate_llimiter/src/App.java
@@ -0,0 +1,34 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/limited", (HttpExchange exchange) -> {
+            String response = "Limited! Don't overuse me!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+
+        server.createContext("/unlimited", (HttpExchange exchange) -> {
+           String response = "Unlimited! let's go!";
+           exchange.sendResponseHeaders(200, response.length());
+           OutputStream os = exchange.getResponseBody();
+           os.write(response.getBytes());
+           os.close();
+        });
+
+        server.start();
+        System.out.println("Server running on port 8080");
+    }
+}
